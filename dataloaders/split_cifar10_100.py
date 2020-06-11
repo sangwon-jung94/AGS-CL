16a065e803af8ce7f20a01dad340fd7f44c64e61
hongjoon0805 <37726120+hongjoon0805@users.noreply.github.com>
2020-06-11 17:22:29 +0900

    make cifar10 directory

--- dataloaders/split_cifar10_100.py
+++ dataloaders/split_cifar10_100.py
@@ -10,18 +10,18 @@ def get(seed=0,pc_valid=0.10, tasknum = 10):
     taskcla=[]
     size=[3,32,32]
 
-    if not os.path.isdir('dat/binary_split_cifar100/'):
-        os.makedirs('dat/binary_split_cifar100')
-        os.makedirs('dat/binary_cifar10')
+    if not os.path.isdir('./dat/binary_split_cifar10/'):
+        # CIFAR10
+        os.makedirs('./dat/binary_cifar10')
 
         mean=[x/255 for x in [125.3,123.0,113.9]]
         std=[x/255 for x in [63.0,62.1,66.7]]
         
         # CIFAR10
         dat={}
-        dat['train']=datasets.CIFAR10('./',train=True,download=True,
+        dat['train']=datasets.CIFAR10('./dat/',train=True,download=True,
                                       transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
-        dat['test']=datasets.CIFAR10('./',train=False,download=True,
+        dat['test']=datasets.CIFAR10('./dat/',train=False,download=True,
                                      transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
         data[0]={}
         data[0]['name']='cifar10'
@@ -34,13 +34,21 @@ def get(seed=0,pc_valid=0.10, tasknum = 10):
                 data[0][s]['x'].append(image)
                 data[0][s]['y'].append(target.numpy()[0])
         
-        
+        # "Unify" and save
+        for s in ['train','test']:
+            data[0][s]['x']=torch.stack(data[0][s]['x']).view(-1,size[0],size[1],size[2])
+            data[0][s]['y']=torch.LongTensor(np.array(data[0][s]['y'],dtype=int)).view(-1)
+            torch.save(data[0][s]['x'], os.path.join(os.path.expanduser('./dat/binary_cifar10'),'data'+s+'x.bin'))
+            torch.save(data[0][s]['y'], os.path.join(os.path.expanduser('./dat/binary_cifar10'),'data'+s+'y.bin'))
+    
+    if not os.path.isdir('./dat/binary_split_cifar100/'):
         # CIFAR100
+        os.makedirs('./dat/binary_split_cifar100')
         dat={}
         
-        dat['train']=datasets.CIFAR100('./',train=True,download=True,
+        dat['train']=datasets.CIFAR100('./dat/',train=True,download=True,
                                        transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
-        dat['test']=datasets.CIFAR100('./',train=False,download=True,
+        dat['test']=datasets.CIFAR100('./dat/',train=False,download=True,
                                        transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
         for n in range(1,11):
             data[n]={}
@@ -55,19 +63,15 @@ def get(seed=0,pc_valid=0.10, tasknum = 10):
                 data[task_idx][s]['x'].append(image)
                 data[task_idx][s]['y'].append(target.numpy()[0]%10)
 
-        # "Unify" and save
-        for s in ['train','test']:
-            data[0][s]['x']=torch.stack(data[0][s]['x']).view(-1,size[0],size[1],size[2])
-            data[0][s]['y']=torch.LongTensor(np.array(data[0][s]['y'],dtype=int)).view(-1)
-            torch.save(data[0][s]['x'], os.path.join(os.path.expanduser('dat/binary_cifar10'),'data'+s+'x.bin'))
-            torch.save(data[0][s]['y'], os.path.join(os.path.expanduser('dat/binary_cifar10'),'data'+s+'y.bin'))
+        
+        
         for t in range(1,11):
             for s in ['train','test']:
                 data[t][s]['x']=torch.stack(data[t][s]['x']).view(-1,size[0],size[1],size[2])
                 data[t][s]['y']=torch.LongTensor(np.array(data[t][s]['y'],dtype=int)).view(-1)
-                torch.save(data[t][s]['x'], os.path.join(os.path.expanduser('dat/binary_split_cifar100'),
+                torch.save(data[t][s]['x'], os.path.join(os.path.expanduser('./dat/binary_split_cifar100'),
                                                          'data'+str(t)+s+'x.bin'))
-                torch.save(data[t][s]['y'], os.path.join(os.path.expanduser('dat/binary_split_cifar100'),
+                torch.save(data[t][s]['y'], os.path.join(os.path.expanduser('./dat/binary_split_cifar100'),
                                                          'data'+str(t)+s+'y.bin'))
     
     # Load binary files
@@ -75,8 +79,8 @@ def get(seed=0,pc_valid=0.10, tasknum = 10):
     data[0] = dict.fromkeys(['name','ncla','train','test'])
     for s in ['train','test']:
         data[0][s]={'x':[],'y':[]}
-        data[0][s]['x']=torch.load(os.path.join(os.path.expanduser('dat/binary_cifar10'),'data'+s+'x.bin'))
-        data[0][s]['y']=torch.load(os.path.join(os.path.expanduser('dat/binary_cifar10'),'data'+s+'y.bin'))
+        data[0][s]['x']=torch.load(os.path.join(os.path.expanduser('./dat/binary_cifar10'),'data'+s+'x.bin'))
+        data[0][s]['y']=torch.load(os.path.join(os.path.expanduser('./dat/binary_cifar10'),'data'+s+'y.bin'))
     data[0]['ncla']=len(np.unique(data[0]['train']['y'].numpy()))
     data[0]['name']='cifar10'
     
@@ -87,9 +91,9 @@ def get(seed=0,pc_valid=0.10, tasknum = 10):
         data[i] = dict.fromkeys(['name','ncla','train','test'])
         for s in ['train','test']:
             data[i][s]={'x':[],'y':[]}
-            data[i][s]['x']=torch.load(os.path.join(os.path.expanduser('dat/binary_split_cifar100'),
+            data[i][s]['x']=torch.load(os.path.join(os.path.expanduser('./dat/binary_split_cifar100'),
                                                     'data'+str(ids[i-1])+s+'x.bin'))
-            data[i][s]['y']=torch.load(os.path.join(os.path.expanduser('dat/binary_split_cifar100'),
+            data[i][s]['y']=torch.load(os.path.join(os.path.expanduser('./dat/binary_split_cifar100'),
                                                     'data'+str(ids[i-1])+s+'y.bin'))
         data[i]['ncla']=len(np.unique(data[i]['train']['y'].numpy()))
         data[i]['name']='cifar100-'+str(ids[i-1])
